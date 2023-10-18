0991cb0fe9ea5277621938efd08bb701a1c7f0d5
MatejLukasMarunica <145869520+MatejLukasMarunica@users.noreply.github.com>
2023-10-18 09:31:24 +0200

    Add files via upload

---
+++ klasaString/klasaString/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasaString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekst = "danas radimo klasu string";
+
+            Console.WriteLine(tekst);
+
+            Console.WriteLine("Broj znakova je:" + tekst.Length);
+
+            Console.WriteLine("Velika slova: " + tekst.ToUpper());
+            Console.WriteLine("Mala slova: " + tekst.ToLower());
+
+            Console.WriteLine(tekst.Substring(0, 5));
+
+            Console.WriteLine(tekst.Substring(13, tekst.Length - 13));
+
+            Console.WriteLine(tekst.Replace("danas", "Sutra"));
+
+            string[] rijeci = tekst.Split(' ');
+            for(int i = 0; i < rijeci.Length; i++)
+            {
+                Console.WriteLine(rijeci[i]); 
+            }
+            foreach(string rijec in rijeci)
+            {
+                Console.WriteLine(rijec);
+            }
+            Console.WriteLine(tekst.Insert(tekst.IndexOf("radimo"), "intenzivno "));
+
+            Console.ReadKey(); 
+        }
+    }
+}
